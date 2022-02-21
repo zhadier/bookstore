5b6c836abc3b0f8ec2ae2f3261cbda304ab7ec50
Zeeshan Haider <zhadier39@gmail.com>
2022-02-22 03:01:33 +0500

    add booklist component for displaying books section

---
+++ src/components/Booklist.js
@@ -0,0 +1,11 @@
+import Book from './Book';
+import AddForm from './AddForm';
+
+const Booklist = () => (
+  <section className="booklist-section">
+    <Book />
+    <AddForm />
+  </section>
+);
+
+export default Booklist;
