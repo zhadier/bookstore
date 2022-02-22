aaf34b0dc421d6154e678701363aefefacfac450
Zeeshan Haider <zhadier39@gmail.com>
2022-02-23 01:26:21 +0500

    update code to be more component oriented

--- src/components/BooklistContainer.js
+++ src/components/BooklistContainer.js
@@ -1,11 +1,11 @@
 import Booklist from './Booklist';
 import AddForm from './AddForm';
 
-const BookListContainer = () => (
+const BooklistContainer = () => (
   <section className="booklist-section">
     <Booklist />
     <AddForm />
   </section>
 );
 
-export default BookListContainer;
+export default BooklistContainer;
