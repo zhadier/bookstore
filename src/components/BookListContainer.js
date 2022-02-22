92ff8ffb6e9943636e60bbc91fd99ce07e8110d7
Zeeshan Haider <zhadier39@gmail.com>
2022-02-22 20:31:26 +0500

    add required changes

---
+++ src/components/BookListContainer.js
@@ -0,0 +1,11 @@
+import Booklist from './Booklist';
+import AddForm from './AddForm';
+
+const BookListContainer = () => (
+  <section className="booklist-section">
+    <Booklist />
+    <AddForm />
+  </section>
+);
+
+export default BookListContainer;
