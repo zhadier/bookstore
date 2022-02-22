11297a40f01e85414e9bc20f19a395ecc8986f03
Zeeshan Haider <zhadier39@gmail.com>
2022-02-23 01:27:54 +0500

    implement loading initial state from local storage

---
+++ src/redux/books/books.js
@@ -0,0 +1,32 @@
+import { loadState } from '../../logic/localStorage';
+// constants
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+
+// initial state
+const initialState = loadState() || [];
+
+// action creators
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (payload) => ({
+  type: REMOVE_BOOK,
+  payload,
+});
+
+// reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
---
+++ src/redux/configureStore.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import booksReducer from './books/books';
+
+const reducer = combineReducers({
+  booksReducer,
+  // additional reducers could be added here
+});
+
+const store = createStore(reducer, applyMiddleware(logger));
+
+export default store;
