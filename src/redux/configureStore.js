d3e037e9e062ee75a642e347f217f6ccfbb91149
Zeeshan Haider <zhadier39@gmail.com>
2022-02-24 03:38:11 +0500

    import thunk and use as middleware in store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import booksReducer from './books/books';
+import thunk from 'redux-thunk';
+import booksReducer, { getBooksFromAPI } from './books/books';
 
 const reducer = combineReducers({
   booksReducer,
   // additional reducers could be added here
 });
 
-const store = createStore(reducer, applyMiddleware(logger));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+store.dispatch(getBooksFromAPI());
 
 export default store;
